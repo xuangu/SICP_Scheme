3b11e40813c8b9baf4b897f5530a1a529c5ef5b2
guxuan <guxuan@hecom.cn>
2017-08-01 18:09:29 +0800

    实现练习2.29的代码

--- 2.2.2.rkt
+++ 2.2.2.rkt
@@ -73,3 +73,76 @@
         ((not (pair? tree)) (cons tree '()))
         (else (append (fringe (car tree))
                       (fringe (cdr tree))))))
+
+
+;; exercise 2.29
+#|
+test case:
+> (define mobile (make-mobile (make-branch 10 20)
+                                  (make-branch 10 20)))
+> (mobile-balance? mobile)
+#t
+> (define unbalance-mobile (make-mobile (make-branch 0 0)
+                                            (make-branch 10 10)))
+> (mobile-balance? unbalance-mobile)
+#f
+|#
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cadr branch))
+
+(define (total-weight mobile)
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+(define (branch-weight branch)
+  (if (hangs-another-mobile? branch)
+      (total-weight (branch-structure branch))
+      (branch-structure branch)))
+
+(define (hangs-another-mobile? branch)
+  (pair? (branch-structure branch)))
+
+(define (mobile-balance? mobile)
+  (let ((left (left-branch mobile))
+        (right (right-branch mobile)))
+    (and
+     (same-torque? left right)
+     (branch-balance? left)
+     (branch-balance? right))))
+
+(define (same-torque? left right)
+  (= (branch-torque left)
+     (branch-torque right)))
+
+(define (branch-balance? branch)
+  (if (hangs-another-mobile? branch)
+      (mobile-balance? (branch-structure branch))
+      #t))
+
+(define (branch-torque branch)
+  (* (branch-length branch)
+     (branch-weight branch)))
+
+(define (deep-for-each proc items)
+  (cond ((null? items) 'done)
+        ((not (pair? items)) (proc items))
+        (else (begin
+                (deep-for-each proc (car items))
+                (deep-for-each proc (cdr items))))))
+
+
