a076f44dd8b356f5dd163ae83fdd61e6923edf42
guxuan <guxuan@hecom.cn>
2017-08-10 19:04:31 +0800

    添加部分练习题答案

--- 2.2.4.rkt
+++ 2.2.4.rkt
@@ -1,4 +1,6 @@
 #lang scheme
+;; test address: http://www.biwascheme.org/demo/pictlang.html
+
 (define (right-split painter n)
   (if (= 0 n)
       painter
@@ -45,10 +47,101 @@
     (combine4 (corner-split painter n))))
 
 ;; exercise 2.45
-(define (split proc1 proc2)
+(define (split big-combiner small-combiner)
   (lambda (painter n)
-    (let ((smaller ((split proc1 proc2) painter (- n 1))))
-      (proc1 painter (proc2 smaller)))))
+    (let ((smaller ((split big-combiner small-combiner) painter (- n 1))))
+      (big-combiner painter (small-combiner smaller)))))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect
+   (+ (xcor-vect v1)
+      (xcor-vect v2))
+   (+ (ycor-vect v1)
+      (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect
+   (- (xcor-vect v1)
+      (xcor-vect v2))
+   (- (ycor-vect v1)
+      (ycor-vect v2))))
+
+(define (scale-vect vect s)
+  (make-vect
+   (* s (xcor-vect vect))
+   (* s (ycor-vect vect))))
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1 frame)
+  (cadr frame))
+
+(define (edge2 frame)
+  (caddr frame))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
+;; exercise 2.48
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+;; exercise 2.49
+(define frame-painter
+  (segments->painter (list (make-segment
+                            (make-vect 0 0)
+                            (make-vect 1 0))
+                           (make-segment
+                            (make-vect 1 0)
+                            (make-vect 1 1))
+                           (make-segment
+                            (make-vect 0 0)
+                            (make-vect 0 1))
+                           (make-segment
+                            (make-vect 0 1)
+                            (make-vect 1 1)))))
+
+(define fork-painter
+  (segments->painter (list (make-segment
+                            (make-vect 0.0 0.0)
+                            (make-vect 1.0 1.0))
+                           (make-segment
+                            (make-vect 1.0 0.0)
+                            (make-vect 0.0 1.0)))))
 
               
       
