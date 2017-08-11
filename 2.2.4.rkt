15f65d65dce202f773f609f265fb2d1fdb1d09a7
guxuan <guxuan@hecom.cn>
2017-08-11 15:59:04 +0800

    添加部分代码

--- 2.2.4.rkt
+++ 2.2.4.rkt
@@ -143,6 +143,109 @@
                             (make-vect 1.0 0.0)
                             (make-vect 0.0 1.0)))))
 
+(define (transform-painter painter origin edge1 edge2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                  new-origin
+                  (sub-vect (m edge1) new-origin)
+                  (sub-vect (m edge2) new-origin)))))))
+
+(define (flip-vect painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (shrink-to-upper-right painter)
+  (tansform-painter painter
+                    (make-vect 0.5 0.5)
+                    (make-vect 0.1 0.5)
+                    (make-vect 0.5 1.0)))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (squash-inwards painter)
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
+
+(define (beside painter1 painter2)
+  (let ((split-pointer (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-pointer
+                              (0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-pointer
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                    (make-vect 1.0 0.0)
+                    (make-vect 0.0 0.0)
+                    (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+;; exercise 2.51
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((bottom (transform-painter painter1
+                                     (make-vect 0.0 0.0)
+                                     (make-vect 1.0 0.0)
+                                     split-point))
+          (top (transform-painter painter2
+                                  split-point
+                                  (make-vect 1.0 0.5)
+                                  (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (bottom frame)
+        (top frame)))))
+
+;; 本解法实质上要搞清楚线性代数的变换规则就可以了，需要一定的线性代数矩阵运算基础
+(define (below painter1 painter2)
+  (lambda (frame)
+    ((flip-horiz
+      (rotate90
+       (beside
+        (rotate270
+         (flip-horiz painter1))
+        (rotate270
+         (flip-horiz painter2)))))
+     frame)))
+          
+      
+
+
+
+
+                     
+
+
+
               
       
           
